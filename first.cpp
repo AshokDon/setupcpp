7a3ee07614c0eba0d944c207e2a722785a4494d1
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-04-05 14:29:03 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void TakeInput(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    TakeInput();
+    
+   
+}
+
+
+
+
+
